f96834c035afa535f879d8367fdda00897e9b41d
YannisTz <i.n.tzortzis@gmail.com>
2022-10-13 15:34:25 +0300

    Add reference to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# This setup file is based on the one found here:
+# https://towardsdatascience.com/create-your-custom-python-package-that-you-can-pip-install-from-your-git-repository-f90465867893
 
 import setuptools
 
